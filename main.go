c883e798147f138154e0fe4c25cf95ab238fa091
George Czabania <george@czabania.com>
2015-05-10 19:29:44 +1200

    Use viper to parse flags

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"regexp"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/mgutz/ansi"
+	"github.com/spf13/cobra"
 	"golang.org/x/net/html"
 )
 
@@ -22,28 +24,73 @@ type Movie struct {
 	Posted      string
 	ViewCount   string
 	Type        string
-	Bitrate     string
-	Resolution  string
+	Bitrate     int
+	ResolutionX int
+	ResolutionY int
+}
+
+func (m Movie) ResolutionArea() int {
+	return m.ResolutionX * m.ResolutionY
 }
 
 func (m Movie) Pretty(id int) string {
 	reset := ansi.ColorCode("reset")
 
-	return fmt.Sprintf("%s%d%s: %s%s%s\n%s%s%s\n%s%s - %s - %s - %s -%s %s - %s%s - %s%s\n",
+	return fmt.Sprintf("%s%d%s: %s%s%s\n%s%s%s\n%s%s - %s - %s - %s -%s %s - %s%d kb/s - %dx%d%s\n",
 		ansi.ColorCode("red:black"), id, reset,
 		ansi.ColorCode("yellow"), m.Title, reset,
 		ansi.ColorCode("white+h"), m.Description, reset,
 		ansi.ColorCode("blue"), m.Duration, m.Size, m.Posted, m.ViewCount, m.Type, reset,
-		ansi.ColorCode("green:black"), m.Bitrate, m.Resolution, reset,
+		ansi.ColorCode("green:black"), m.Bitrate, m.ResolutionX, m.ResolutionY, reset,
 	)
 }
 
-func main() {
-	query := strings.Join(os.Args[1:], " ")
-	Search("\"" + strings.Replace(query, " ", "+", -1) + "\"")
+type ByQuality []*Movie
+
+func (a ByQuality) Len() int      { return len(a) }
+func (a ByQuality) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByQuality) Less(i, j int) bool {
+	if a[i].ResolutionArea() == a[j].ResolutionArea() {
+		return a[i].Bitrate < a[j].Bitrate
+	}
+	return a[i].ResolutionArea() < a[j].ResolutionArea()
 }
 
-func Search(query string) {
+func main() {
+
+	var searchTerm string
+	var atIndex int
+	var sortByQuality bool
+	var wrapQuotes bool
+
+	var VeehdCmd = &cobra.Command{
+		Use:   "veehd",
+		Short: "a command line interface for veehd.com",
+		Run: func(cmd *cobra.Command, args []string) {
+			searchTerm = strings.Replace(searchTerm, " ", "+", -1)
+			if wrapQuotes {
+				searchTerm = "\"" + searchTerm + "\""
+			}
+			Search(searchTerm, atIndex, sortByQuality)
+		},
+	}
+
+	VeehdCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Print the version number",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("veehd v0.1 -- HEAD")
+		},
+	})
+
+	VeehdCmd.Flags().StringVarP(&searchTerm, "search", "s", "", "Term to search for")
+	VeehdCmd.Flags().IntVarP(&atIndex, "get", "g", -1, "Don't display results, just return url for selected video")
+	VeehdCmd.Flags().BoolVarP(&sortByQuality, "sort", "t", false, "Sort movies by quality (low to high)")
+	VeehdCmd.Flags().BoolVar(&wrapQuotes, "quotes", true, "Wrap search term in quotes")
+	VeehdCmd.Execute()
+}
+
+func Search(query string, index int, sortByQuality bool) {
 	url := "http://veehd.com/search?q=" + query
 
 	doc, err := goquery.NewDocument(url)
@@ -77,15 +124,34 @@ func Search(query string) {
 	})
 
 	if len(results) <= 0 {
-		fmt.Println("No results found for", query)
+		log.Fatal("No results found for", query)
+		return
+	}
+
+	if index > -1 {
+		if index >= len(results) {
+			log.Fatal("Invalid index")
+		}
+
+		GetDownloadLink(results[index])
 		return
 	}
 
 	fmt.Printf("Found %d results...\n", len(results))
 
-	for i, result := range results {
-		ScrapeMovie(result)
-		fmt.Println(result.Pretty(i))
+	if sortByQuality {
+		for _, result := range results {
+			ScrapeMovie(result)
+		}
+		sort.Sort(ByQuality(results))
+		for i, result := range results {
+			fmt.Println(result.Pretty(i))
+		}
+	} else {
+		for i, result := range results {
+			ScrapeMovie(result)
+			fmt.Println(result.Pretty(i))
+		}
 	}
 
 	fmt.Println("Select movie: ")
@@ -100,8 +166,8 @@ func Search(query string) {
 }
 
 var VpiRegex = regexp.MustCompile("\"/vpi?.+do=d.+\"")
-var BitrateRegex = regexp.MustCompile("bitrate: (\\d* kb/s)")
-var ResolutionRegex = regexp.MustCompile("resolution: (\\d*x\\d*)")
+var BitrateRegex = regexp.MustCompile("bitrate: (\\d*) kb/s")
+var ResolutionRegex = regexp.MustCompile("resolution: (\\d*)x(\\d*)")
 var TypeRegex = regexp.MustCompile("type: (\\w+)")
 
 func ScrapeMovie(m *Movie) {
@@ -111,9 +177,17 @@ func ScrapeMovie(m *Movie) {
 	}
 
 	details := doc.Find(".info > table > tbody > tr > td:nth-child(2) > div").Text()
-	m.Bitrate = BitrateRegex.FindStringSubmatch(details)[1]
-	m.Resolution = ResolutionRegex.FindStringSubmatch(details)[1]
 	m.Type = TypeRegex.FindStringSubmatch(details)[1]
+
+	var bitrate string = BitrateRegex.FindStringSubmatch(details)[1]
+	m.Bitrate, err = strconv.Atoi(bitrate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var resolution []string = ResolutionRegex.FindStringSubmatch(details)
+	m.ResolutionX, _ = strconv.Atoi(resolution[1])
+	m.ResolutionY, _ = strconv.Atoi(resolution[2])
 
 	// http://stackoverflow.com/a/3442757
 	desc := doc.Find(".info > table > tbody > tr > td:nth-child(3) > span > div")
